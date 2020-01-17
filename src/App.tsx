1f60d536dd0499140af38bb6f7d7ca0133e9d7f8
Ivan Čurić <ivan.curic@gmail.com>
2020-01-17 15:04:21 +0100

    fast reload on

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-const App: React.FC = () => {
+const API_KEY = "AIzaSyDCfKSNLeYj2DCufI9nfMTc_-gtpyWMW9M";
+
+const App = () => {
+  const loadYoutubeApi = () => {
+    if (window.gapi) {
+      return;
+    }
+
+    const script = document.createElement("script");
+    script.src = "https://apis.google.com/js/client.js";
+
+    script.onload = () => {
+      window.gapi.load("client", () => {
+        window.gapi.client.setApiKey(API_KEY);
+        window.gapi.client.load("youtube", "v3", () => {
+          console.log("Google client api loaded");
+        });
+      });
+    };
+
+    document.body.appendChild(script);
+  };
+
+  // function authenticate() {
+  //   return window.gapi.auth2
+  //     .getAuthInstance()
+  //     .signIn({ scope: "https://www.googleapis.com/auth/youtube.readonly" })
+  //     .then(
+  //       function() {
+  //         console.log("Sign-in successful");
+  //       },
+  //       function(err: any) {
+  //         console.error("Error signing in", err);
+  //       }
+  //     );
+  // }
+  // // Make sure the client is loaded and sign-in is complete before calling this method.
+  // function execute() {
+  //   return window.gapi.client.youtube.videos
+  //     .list({
+  //       part: "snippet,contentDetails,statistics",
+  //       chart: "mostPopular",
+  //       regionCode: "US"
+  //     })
+  //     .then(
+  //       function(response: any) {
+  //         // Handle the results here (response.result has the parsed body).
+  //         console.log("Response", response);
+  //       },
+  //       function(err: any) {
+  //         console.error("Execute error", err);
+  //       }
+  //     );
+  // }
+
+  useEffect(() => loadYoutubeApi(), []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +77,6 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-}
+};
 
 export default App;
