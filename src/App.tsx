678943fc96aa2f355609d4c1e60d9c232cc2cf2b
Ivan Čurić <ivan.curic@gmail.com>
2020-01-21 14:48:56 +0100

    RADI JEBEMU

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import React, { useEffect } from "react";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useEffect, useRef, useCallback, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 const API_KEY = "AIzaSyDCfKSNLeYj2DCufI9nfMTc_-gtpyWMW9M";
+const CLIENT_ID =
+  "332020508961-t7tue80uk5ovr7it0j6513plf450tfmv.apps.googleusercontent.com";
 
 const App = () => {
-  const loadYoutubeApi = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const loadYoutubeApi = useCallback(() => {
     if (window.gapi) {
       return;
     }
@@ -15,46 +19,50 @@ const App = () => {
 
     script.onload = () => {
       window.gapi.load("client", () => {
-        window.gapi.client.setApiKey(API_KEY);
         window.gapi.client.load("youtube", "v3", () => {
           console.log("Google client api loaded");
+          init();
         });
       });
     };
 
     document.body.appendChild(script);
+  }, []);
+
+  const init = () => {
+    gapi.client.setApiKey(API_KEY);
+    gapi.auth2.init({
+      client_id: CLIENT_ID,
+      scope: "https://www.googleapis.com/auth/youtube.readonly"
+    });
   };
 
-  function authenticate() {
-    return window.gapi.auth2
+  const authenticate = async () => {
+    window.gapi.auth2
       .getAuthInstance()
-      .signIn({ scope: "https://www.googleapis.com/auth/youtube.readonly" })
+      .signIn()
       .then(
-        () => console.log("Sign-in successful"),
-        (err: Error) => console.error("Error signing in", err)
+        () => setIsLoggedIn(true),
+        err => console.error("Error signing in", err)
       );
-  }
+  };
 
-  // // Make sure the client is loaded and sign-in is complete before calling this method.
-  function execute() {
-    return window.gapi.client.youtube.videos
-      .list({
+  // Make sure the client is loaded and sign-in is complete before calling this method.
+  const execute = async () => {
+    try {
+      const response = await window.gapi.client.youtube.videos.list({
         part: "snippet,contentDetails,statistics",
         chart: "mostPopular",
         regionCode: "US"
-      })
-      .then(
-        function(response: any) {
-          // Handle the results here (response.result has the parsed body).
-          console.log("Response", response);
-        },
-        function(err: any) {
-          console.error("Execute error", err);
-        }
-      );
-  }
+      });
+      // Handle the results here (response.result has the parsed body).
+      console.log("Response", response);
+    } catch (err) {
+      return console.error("Execute error", err);
+    }
+  };
 
-  useEffect(() => loadYoutubeApi(), []);
+  useEffect(() => loadYoutubeApi(), [loadYoutubeApi]);
 
   return (
     <div className="App">
@@ -63,14 +71,8 @@ const App = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => authenticate()}>Authenticate</button>
+        {isLoggedIn && <button onClick={() => execute()}>Fetch Videos</button>}
       </header>
     </div>
   );
