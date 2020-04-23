4f4687987b38577841bce0aa18ad524191df2abc
Ivan Čurić <ivan.curic@gmail.com>
2020-04-23 13:52:09 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import * as React from "react";
-import { usePaginatedQuery, useInfiniteQuery } from "react-query";
+import { useInfiniteQuery } from "react-query";
 import { ReactQueryDevtools } from "react-query-devtools";
 import "./App.css";
 import { useGapi } from "./useGapi";
 
-const getPopularVideos = async (key: string, pageToken?: string) => {
-  try {
-    const { result } = await window.gapi.client.youtube.videos.list({
-      part: "snippet",
-      chart: "mostPopular",
-      regionCode: "US",
-      maxResults: 10,
-      ...{ pageToken },
-    });
-
-    return result;
-  } catch (error) {
-    throw error.result.error;
-  }
-};
-
 const App = () => {
   const gapiLoaded = useGapi();
   const [pageToken, setPageToken] = React.useState();
+
+  const getPopularVideos = async (key: string, pageToken?: string) => {
+    try {
+      const { result } = await window.gapi.client.youtube.videos.list({
+        part: "snippet",
+        chart: "mostPopular",
+        regionCode: "US",
+        maxResults: 10,
+        ...{ pageToken },
+      });
+
+      return result;
+    } catch (error) {
+      throw error.result.error;
+    }
+  };
 
   const {
     status,
@@ -34,13 +34,13 @@ const App = () => {
     fetchMore,
     canFetchMore,
   } = useInfiniteQuery(gapiLoaded && "videos", getPopularVideos, {
-    getFetchMore: (lastGroup, allGroups) => lastGroup.nextCursor,
-    // retry: (i, result: any) => {
-    //   if (result.code === 403) {
-    //     return false;
-    //   }
-    //   return true;
-    // },
+    getFetchMore: (lastGroup, allGroups) => lastGroup.nextPageToken,
+    retry: (i, error) => {
+      if (error.code === 403) {
+        return false;
+      }
+      return true;
+    },
     // staleTime: 100000,
   });
 
@@ -50,9 +50,25 @@ const App = () => {
         {status === "loading" && <div>Loading...</div>}
         {status === "error" && <div>Error: {(error as any).message}</div>}
         {status === "success" &&
-          data.items?.map((item) => (
-            <div key={item.id}>{item?.snippet?.title}</div>
+          data.map((group, i) => (
+            <React.Fragment key={i}>
+              {group?.items?.map((item) => (
+                <div key={item.id}>{item?.snippet?.title}</div>
+              ))}
+            </React.Fragment>
           ))}
+        <div>
+          <button
+            onClick={() => fetchMore()}
+            disabled={!canFetchMore || isFetchingMore}
+          >
+            {isFetching || isFetchingMore
+              ? "Loading more..."
+              : canFetchMore
+              ? "Load More"
+              : "Nothing more to load"}
+          </button>
+        </div>
       </div>
       <ReactQueryDevtools initialIsOpen={true} />
     </>
